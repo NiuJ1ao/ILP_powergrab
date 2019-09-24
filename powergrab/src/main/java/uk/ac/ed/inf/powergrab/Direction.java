56047b086f2215c446d44e1a804f46725ff8ef7b
NiuJ1ao <niuyuchen666@sina.com>
2019-09-24 17:12:50 +0100

    Position & Direction complete

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,20 +1,20 @@
 package uk.ac.ed.inf.powergrab;
 
-public final class Direction {
-	public static final double E = 0;
-	public static final double ENE = 22.5;
-	public static final double NE = 45;
-	public static final double NNE = 67.5;
-	public static final double N = 90;
-	public static final double NNW = 112.5;
-	public static final double NW = 135;
-	public static final double WNW = 157.5;
-	public static final double W = 180;
-	public static final double WSW = 202.5;
-	public static final double SW = 225;
-	public static final double SSW = 247.5;
-	public static final double S = 270;
-	public static final double SSE = 292.5;
-	public static final double SE = 315;
-	public static final double ESE = 337.5;
+public enum Direction {
+	E,
+	ENE,
+	NE,
+	NNE,
+	N,
+	NNW,
+	NW,
+	WNW,
+	W,
+	WSW,
+	SW,
+	SSW,
+	S,
+	SSE,
+	SE,
+	ESE,
 }	
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -4,7 +4,7 @@ public class Position {
 	
 	public double latitude;
 	public double longitude;
-	
+	private final double r = 0.0003;
 	
 	public Position (double latitude, double longitude) {
 		this.latitude = latitude;
@@ -12,10 +12,57 @@ public class Position {
 	}
 	
 	public Position nextPosition(Direction direction) {
-		return null;
+		double degree = directionToDegree(direction);
+		
+		double newLatitude = r * Math.sin(degree);
+		double newLongitude = r * Math.cos(degree);
+		
+		Position nextPos = new Position(newLatitude, newLongitude);
+		
+		return nextPos;				
 	}
 	
 	public boolean inPlayArea() {
-		return true;
+		return (latitude >= 5.942617 && latitude <= 55.94623
+				&& longitude >= -3.184319 && longitude <= -3.19247);
+	}
+	
+	private double directionToDegree(Direction direction ) {
+		switch (direction) {
+		case E:
+			return 0;
+		case ENE:
+			return 22.5;
+		case NE:
+			return 45;
+		case NNE:
+			return 67.5;
+		case N:
+			return 90;
+		case NNW:
+			return 112.5;
+		case NW:
+			return 135;
+		case WNW:
+			return 157.5;
+		case W:
+			return 180;
+		case WSW:
+			return 202.5;
+		case SW:
+			return 225;
+		case SSW:
+			return 247.5;
+		case S:
+			return 270;
+		case SSE:
+			return 292.5;
+		case SE:
+			return 315;
+		case ESE:
+			return 337.5;
+		default:
+			return 0;
+		}	
 	}
 }
