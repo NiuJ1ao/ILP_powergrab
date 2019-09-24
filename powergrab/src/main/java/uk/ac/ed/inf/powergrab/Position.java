ce7ab83280d1f6f8fb015856f6c0b80efaec87a3
NiuJ1ao <niuyuchen666@sina.com>
2019-09-24 18:03:45 +0100

    Passed Junit test -negative number and radius

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -14,8 +14,8 @@ public class Position {
 	public Position nextPosition(Direction direction) {
 		double degree = directionToDegree(direction);
 		
-		double newLatitude = r * Math.sin(degree);
-		double newLongitude = r * Math.cos(degree);
+		double newLatitude = latitude + r * Math.sin(degree);
+		double newLongitude = longitude + r * Math.cos(degree);
 		
 		Position nextPos = new Position(newLatitude, newLongitude);
 		
@@ -23,44 +23,50 @@ public class Position {
 	}
 	
 	public boolean inPlayArea() {
-		return (latitude >= 5.942617 && latitude <= 55.94623
-				&& longitude >= -3.184319 && longitude <= -3.19247);
+		double MINLATITUDE = 55.942617;
+		double MAXLATITUDE = 55.946233;
+		double MAXLONGITUDE = -3.184319;
+		double MINLONGITUDE = -3.192473;
+		
+		return (latitude > MINLATITUDE && latitude < MAXLATITUDE 
+				&& longitude > MINLONGITUDE && longitude < MAXLONGITUDE);
 	}
 	
-	private double directionToDegree(Direction direction ) {
+	public double directionToDegree(Direction direction ) {
+		double pi = Math.PI;
 		switch (direction) {
 		case E:
 			return 0;
 		case ENE:
-			return 22.5;
+			return pi/8;
 		case NE:
-			return 45;
+			return pi/4;
 		case NNE:
-			return 67.5;
+			return 3*pi/8;
 		case N:
-			return 90;
+			return pi/2;
 		case NNW:
-			return 112.5;
+			return 5*pi/8;
 		case NW:
-			return 135;
+			return 3*pi/4;
 		case WNW:
-			return 157.5;
+			return 7*pi/8;
 		case W:
-			return 180;
+			return pi;
 		case WSW:
-			return 202.5;
+			return 9*pi/8;
 		case SW:
-			return 225;
+			return 5*pi/4;
 		case SSW:
-			return 247.5;
+			return 11*pi/8;
 		case S:
-			return 270;
+			return 3*pi/2;
 		case SSE:
-			return 292.5;
+			return 13*pi/8;
 		case SE:
-			return 315;
+			return 7*pi/4;
 		case ESE:
-			return 337.5;
+			return 15*pi/8;
 		default:
 			return 0;
 		}	
