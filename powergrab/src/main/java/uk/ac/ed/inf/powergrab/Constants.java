98f11bfe6f862506c9ecbeaaea5ff5da822bd2b6
NiuJ1ao <NiuJ1ao@github.com>
2019-11-02 01:40:41 +0000

    emmm A*

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Constants.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Constants.java
@@ -4,7 +4,5 @@ public class Constants {
 	static final double POWER_CONSUMPTION = 1.25;
 	static final double MINPAYLOAD = 0.0;
 	static final double MAXPOWER = 250;
-    static final int LIGHTHOUSE = 1;
-	static final int SKULL = 0;
 	static final double ACCESS_RANGE = 0.00025;
 }
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
@@ -1,7 +1,9 @@
 package uk.ac.ed.inf.powergrab;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.JsonObject;
 import com.mapbox.geojson.Feature;
@@ -27,8 +29,52 @@ public class StatefulDrone extends Drone{
 		return f;
 	}
 	
-	// Route planner: DFS / MCTS
-	// distance to skull = +inf
-	// Driver
-
+	private Position[] astar(Position drone, ChargingStation station) {
+		Map<Position, Double> g_scores = new HashMap<Position, Double>();
+		List<Position> openSet = new ArrayList<Position>();
+		Map<Position, Double> f_scores = new HashMap<Position, Double>();
+		Map<Position, Position> cameFrom = new HashMap<Position, Position>();
+		
+		Direction[] directions = Direction.values();
+		
+		f_scores.put(drone, station.distanceToDrone(drone));
+		openSet.add(drone);
+		g_scores.put(drone, 0.);
+		while (!openSet.isEmpty()) {
+			Position cur = openSet.get(0);
+			for (Position p : openSet) {
+				if (f_scores.containsKey(p) && f_scores.get(p) < f_scores.get(cur)) {
+					cur = p;
+				};
+			}
+			
+			if (station.distanceToDrone(cur) <= Constants.ACCESS_RANGE) {
+				return reconstruct_path(cameFrom, cur);
+			}
+			
+			List<Position> neighbors = findNeighbors(cur);
+			for (Position neighbor : neighbors) {
+				double tentative_g = g_scores.get(cur) + Position.r;
+				if (!openSet.contains(neighbor)) {
+					cameFrom.put(neighbor, cur);
+					f_scores.put(neighbor, tentative_g + station.distanceToDrone(cur));
+					g_scores.put(neighbor, tentative_g);
+					openSet.add(neighbor);
+				} else {
+					if (g_scores.containsKey(neighbor) && tentative_g < g_scores.get(neighbor)) {
+						cameFrom.replace(neighbor, cur);
+						g_scores.replace(neighbor, tentative_g);
+						f_scores.replace(neighbor, tentative_g + station.distanceToDrone(cur));
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	private Position[] reconstruct_path(Map<Position, Position> cameFrom, Position current) {
+		return null;
+	}
+	
 }
